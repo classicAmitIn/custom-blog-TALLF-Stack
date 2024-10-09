094dc4ef17df77024f0eda476c701ca6fd9c9973
Amit Yadav <email.ay@proton.me>
2024-10-09 18:34:16 +0530

    updated with articles and setSort function

--- app/Livewire/ArticleList.php
+++ app/Livewire/ArticleList.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ArticleList extends Component
 {
+    use WithPagination;
+
+    #[Url()]
+    public $sort = 'desc';
+
+    public function setSort($sort)
+    {
+        $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+    }
+
+    #[Computed()]
+    public function articles()
+    {
+        return Article::published()->orderBy('published_at', $this->sort)->simplePaginate(9);
+    }
+
     public function render()
     {
         return view('livewire.article-list');

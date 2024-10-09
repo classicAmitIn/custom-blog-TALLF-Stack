a0a0450390799495800254c11e2b164e802eb88a
Amit Yadav <email.ay@proton.me>
2024-10-09 18:30:22 +0530

    updated livewire component to display articles

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div class=" px-3 lg:px-7 py-6">
+    <div class="flex justify-between items-center border-b border-gray-100">
+        <div class="flex items-center space-x-4 font-light ">
+            <button class="text-gray-500 py-4">Latest</button>
+            <button class="text-gray-900 py-4 border-b border-gray-700">Oldest</button>
+        </div>
+    </div>
+    <div class="py-4">
+        @foreach ($this->articles as $article)
+            <x-custom.latest-article :article="$article" />
+        @endforeach
+    </div>
+
+    <div class="my-3">
+        {{ $this->articles->links() }}
+    </div>
 </div>

1a4088331ce55669db948850dea48b53bf4f0351
Amit Yadav <email.ay@proton.me>
2024-10-09 08:27:29 +0530

    added fillables and relationships

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,11 +2,65 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
     /** @use HasFactory<\Database\Factories\ArticleFactory> */
     use HasFactory;
+    use SoftDeletes;
+
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'slug',
+        'title',
+        'summary',
+        'body',
+        'published_at',
+        'featured_image',
+        'featured_image_caption',
+        'is_featured',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function scopePublished($query)
+    {
+        $query->where('published_at', '<=', Carbon::now());
+    }
+
+    public function scopeFeatured($query)
+    {
+        $query->where('is_featured', true);
+    }
+
+     public function getReadingTime()
+    {
+        $mins = round(str_word_count($this->body) / 250);
+
+        return ($mins < 1) ? 1 : $mins;
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'text_color',
+        'bg_color',
+        'is_active'
+    ];
+
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function scopeActive($query)
+    {
+        $query->where('is_active', true);
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
 }

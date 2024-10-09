4644f38364932764089ab7097da527e6258aece1
Amit Yadav <email.ay@proton.me>
2024-10-09 08:15:44 +0530

    added columns to migration

--- database/migrations/2024_10_09_081346_create_categories_table.php
+++ database/migrations/2024_10_09_081346_create_categories_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('text_color')->nullable();
+            $table->string('bg_color')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

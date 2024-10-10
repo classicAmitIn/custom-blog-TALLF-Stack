af56334f58c37071294e586b6d27f489a2fc6892
Amit Yadav <email.ay@proton.me>
2024-10-10 05:46:19 +0530

    updated for search

--- app/Livewire/ArticleList.php
+++ app/Livewire/ArticleList.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -15,15 +16,25 @@ class ArticleList extends Component
     #[Url()]
     public $sort = 'desc';
 
+    #[Url()]
+    public $search = '';
+
     public function setSort($sort)
     {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
     }
 
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+        $this->resetPage();
+    }
+
     #[Computed()]
     public function articles()
     {
-        return Article::published()->orderBy('published_at', $this->sort)->simplePaginate(9);
+        return Article::published()->where('title', 'like', "%{$this->search}%")->orderBy('published_at', $this->sort)->simplePaginate(9);
     }
 
     public function render()

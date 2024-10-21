5616f7ae3a57c8b7d92e7dd7b7407d6272a285b1
Amit Yadav <email.ay@proton.me>
2024-10-21 21:03:32 +0530

    added caching

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Cache;
 
 class WelcomeController extends Controller
 {
@@ -13,9 +14,22 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request): View
     {
+        // return view('welcome', [
+        //     'featuredArticles' => Article::published()->featured()->with('category')->latest('published_at')->take(3)->get(),
+        //     'latestArticles' => Article::published()->with('user', 'category')->latest('published_at')->take(9)->get(),
+        // ]);
+
+        $featuredArticles = Cache::remember('featuredArticles', now()->addDay(), function () {
+            return Article::published()->featured()->with('category')->latest('published_at')->take(3)->get();
+        });
+
+        $latestArticles = Cache::remember('latestArticles', now()->addDay(), function () {
+            return Article::published()->with('user', 'category')->latest('published_at')->take(9)->get();
+        });
+
         return view('welcome', [
-            'featuredArticles' => Article::published()->featured()->with('category')->latest('published_at')->take(3)->get(),
-            'latestArticles' => Article::published()->with('user', 'category')->latest('published_at')->take(9)->get(),
+            'featuredArticles' => $featuredArticles,
+            'latestArticles' => $latestArticles
         ]);
     }
 }
